8437af8b04e10f820e277f3e3d299b855314b2bd
erickmwei680@gmail.com <erickmwei680@gmail.com>
2024-03-12 13:56:42 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ const App = () => {
         </div>
       ) : (
         <div className="empty">
-          <h2>No movies found</h2>
+          <h2>No movies found!</h2>
         </div>
       )}
     </div>
